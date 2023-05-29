fecfc19d0ff7dc7870eff952c431aacc292ef68a
Guilherme Alexandrino~ <alexandrinoguilherme@hotmail.com>
2023-05-29 15:28:09 -0300

    primeiro commit

---
+++ cadastrar.php
@@ -0,0 +1,10 @@
+<h1>Cadastrar Usuário</h1>
+<form method="POST" action="cadastrar_action.php">    
+    <label for="">
+        Nome <input type="text" name="nome"> 
+    </label>
+    <label for="">
+        Email <input type="email" name="email">
+    </label>
+    <input type="submit" value="Enviar">
+</form>
